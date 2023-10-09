67149befaf4ff95243316de4103d088378be3f46
Nathan Nowack <thrast36@gmail.com>
2023-10-09 13:45:23 -0500

    no fields

--- src/marvin_recipes/settings.py
+++ src/marvin_recipes/settings.py
@@ -1,16 +1,16 @@
 from typing import Any
 
 from marvin.settings import MarvinBaseSettings
-from pydantic import Field, SecretStr
+from pydantic import SecretStr
 
 
 class ChromaSettings(MarvinBaseSettings):
     """Provider-specific settings. Only some of these will be relevant to users."""
 
-    chroma_db_impl: str | None = Field(None)
-    chroma_server_host: str = Field("localhost")
-    chroma_server_http_port: int = Field(8000)
-    is_persistent: bool = Field(True)
+    chroma_db_impl: str | None = None
+    chroma_server_host: str = "localhost"
+    chroma_server_http_port: int = 8000
+    is_persistent: bool = True
 
     def __getitem__(self, key: str) -> Any:
         return getattr(self, key)
